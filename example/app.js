b4600454f88c93c14c17a3b852a6616fd76c7df9
Zsolt Boda <boda0zsolt@live.com>
2019-07-17 22:40:54 +0200

    v1

--- example/app.js
+++ example/app.js
@@ -1,5 +1,20 @@
 import { Drive } from "../src/drive.js";
 
+let key = new URL(window.location.href).searchParams.get("key");
+let id = new URL(window.location.href).searchParams.get("folderID");
+
+if (id !== null && key !== null) {
+	document.getElementById("key").value = key;
+	document.getElementById("folderID").value = id;
+
+	Drive.Key = key;
+	Drive.GetFiles(id);
+}
+
 document.addEventListener("loadedFilesList", (e) => {
-	
+	let r = "";
+	e.detail.files.forEach((f) => {
+		r += `${f.id} -> ${f.webContentLink}\n`;
+	});
+	document.getElementById("result").innerText = r;
 });
